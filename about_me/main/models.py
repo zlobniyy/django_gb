bcdbdb583b610be7768726c134968f8c648e1c01
Кравченко Степан Борисович <skravchenko@win.ftc.ru>
2017-02-13 16:59:21 +0700

    ---homework3 -сделано без *

--- about_me/main/models.py
+++ about_me/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Work(models.Model):
+    organization = models.CharField(verbose_name='Организация', max_length=32)
+    region = models.CharField(verbose_name='Регион', max_length=32, blank=True)
+    site = models.CharField(verbose_name='Сайт', max_length=64, blank=True)
+    position = models.CharField(verbose_name='Должность', max_length=16)
+    duties = models.TextField(verbose_name='Обязанности')
+    period = models.PositiveIntegerField(verbose_name='Время работы', default=1)
+
+
+class Study(models.Model):
+    schools = models.CharField(verbose_name='Наименование учебного учреждения', max_length=32)
+    site = models.CharField(verbose_name='Сайт Учреждения', max_length=64, blank=True)
+    course = models.CharField(verbose_name='Курс/Специальность', max_length=16)
+    period = models.PositiveIntegerField(verbose_name='Длительность обучения(месяцов)', default=0)
+
+
+class Aboutme(models.Model):
+    hobbyname = models.CharField(verbose_name='Хобби', max_length=32)
+    region = models.CharField(verbose_name='Регион', max_length=32, blank=True)
+    site = models.CharField(verbose_name='Образцы работ', max_length=64, blank=True)
+    hobbylevel = models.CharField(verbose_name='Сила увлечения', max_length=16)
--- about_me/main/views.py
+++ about_me/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 import time, datetime
+from main.models import Work
+from main.models import Study
+from main.models import Aboutme
 
 
 # Create your views here.
@@ -19,30 +22,30 @@ def aboutme(request):
     car = 'почти 18-летний старый Subaru Impreza. :)'
     name = 'Степан'
     title = 'Обо мне'
-    return render(request, "aboutme.html", {'age': age, 'car': car, 'name': name, 'title': title, 'page': page, 'wasborn':wasborn})
+    hobby_list = Aboutme.objects.all() or ['Я ничем не увлекаюсьобожешьмой 0_о']
+    return render(request, "aboutme.html", {'age': age,
+                                            'car': car,
+                                            'name': name,
+                                            'title': title,
+                                            'page': page,
+                                            'wasborn': wasborn,
+                                            'hobbys': [aboutme.hobbyname for aboutme in hobby_list]})
 
 
 def study(request):
     page = 'study'
-    schools = ['Высшее техническое образование по автоматизации проектирования в машиностроении',
-               'Курсы Oracle DBA level 1&2', 'Курс Oracle SQL Tuning',
-               'Linux(Debian-family)', 'Unix Solaris', 'Shell',
-               'Так как последние пару лет работал в сопровождении банка, то бухучет знаком',
-               'HTML/CSS+Bootstrap(курс GeekBrains)', 'JS(курс на GeekBrains в процессе)',
-               'Python(курс на GeekBrains в процессе)', 'В общении с людьми не испытваю проблем'] or ['Видимо я ничего не умею 0_о']
+    schools_list = Study.objects.all() or ['Видимо я ничего не умею 0_о']
     title = 'Учеба'
-    return render(request, 'study.html', {'schools': schools, 'title': title, 'page': page})
+    return render(request, 'study.html',
+                  {'schools': [study.schools + " - " + study.course for study in schools_list], 'title': title,
+                   'page': page})
 
 
 def work(request):
     page = 'work'
-    works = ['Вожатый', 'Мастер починки сотовых телефонов',
-             'Эникейщик',
-             'Сисадмин',
-             'Инженер прикладного сопровождения',
-             'Прикладной администратор',
-             'Фотограф на соревноваяних RDS Сибирь.'] or ['Здесь пока ничего нет']
-
+    works_list = Work.objects.all() or ['Здесь пока ничего нет']
 
     title = 'Работа'
-    return render(request, 'work.html', {'works': works, 'title': title, 'page': page})
+    return render(request, 'work.html',
+                  {'works': [work.organization + " - " + work.position for work in works_list], 'title': title,
+                   'page': page})
